8dbdd43ffde2a148bd294f67ca3e45b985670b29
abmarghoub <a.marghoub5257@uca.ac.ma>
2025-11-24 00:13:10 +0100

    Navigation Drawer

---
+++ navgationDraw/app/src/main/java/ma/ens/navgationdraw/MainActivity.java
@@ -0,0 +1,101 @@
+package ma.ens.navgationdraw;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import ma.ens.navgationdraw.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // ViewBinding
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Toolbar
+        setSupportActionBar(binding.appBarMain.toolbar);
+
+        // Floating Action Button
+        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null)
+                        .setAnchorView(R.id.fab)
+                        .show();
+            }
+        });
+
+        // DrawerLayout et NavigationView
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+
+        // Lier le NavigationView au listener pour gérer les clics
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // Ajouter le toggle pour ouvrir/fermer le Drawer
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawer,
+                binding.appBarMain.toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    // Gestion des clics sur les items du Drawer
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_fragment1) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.contenu, new BlankFragment())
+                    .commit();
+        } else if (id == R.id.nav_fragment2) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.contenu, new BlankFragment2())
+                    .commit();
+        } else if (id == R.id.nav_list) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.contenu, new FragmentList())
+                    .commit();
+        }
+
+        binding.drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
